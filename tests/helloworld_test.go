25afa083d82ad8e12add33f531ab43d7d09ed983
shahab-github <113800863+shahab-github@users.noreply.github.com>
2023-08-26 18:07:49 -0400

    Update helloworld_test.go

--- tests/helloworld_test.go
+++ tests/helloworld_test.go
@@ -12,7 +12,7 @@ func TestTerraformHelloWorldExample(t *testing.T) {
 	// retryable errors in terraform testing.
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
 		// Set the path to the Terraform code that will be tested.
-		TerraformDir: "../examples/terraform-hello-world-example",
+		TerraformDir: "../",
 	})
 
 	// Clean up resources with "terraform destroy" at the end of the test.
